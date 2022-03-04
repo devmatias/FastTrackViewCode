ba2fa303e83c42b388d9976676a227aae1159ad7
devmatias <devmatias@gmail.com>
2022-03-04 13:24:47 -0300

    Initial Commit

---
+++ FastTrackViewCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FastTrackViewCode
+//
+//  Created by Matias Correa Franco de Faria on 04/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
